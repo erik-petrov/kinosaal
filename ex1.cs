d0ea9e3002ca28748fbabe9698fb0c25b39288be
Opilane TTHK <opilane@tthk.ee>
2021-10-20 10:43:00 +0300

    Add project files.

---
+++ ex1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variant7ulesanne3
+{
+    class ex1
+    {
+        public static void Startex1()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int[,] medium = new int[20, 30];
+            int[,] small = new int[10, 20];
+            int[,] big = new int[50, 70];
+
+            Random rnd = new Random();
+            for (int i = 0; i < 15; i++)
+            {
+                medium[rnd.Next(medium.GetLength(0)), rnd.Next(medium.GetLength(1))] = 1;
+                small[rnd.Next(small.GetLength(0)), rnd.Next(small.GetLength(1))] = 1;
+                big[rnd.Next(big.GetLength(0)), rnd.Next(big.GetLength(1))] = 1;
+            }
+            while (true)
+            {
+                Console.WriteLine("Впишите, какой зал хотите выбрать(big, med, small): ");
+                string dec = Console.ReadLine();
+                switch (dec)
+                {
+                    case "big":
+                        printAr(big);
+                        buy(big);
+                        break;
+                    case "med":
+                        printAr(medium);
+                        buy(medium);
+                        break;
+                    case "small":
+                        printAr(small);
+                        buy(small);
+                        break;
+                    case "q":
+                        goto q;
+                    default:
+                        Console.WriteLine("Неверный зал.");
+                        break;
+                }
+            }
+        q:
+            Console.WriteLine("Всего доброго.");
+            Console.ReadLine();
+        }
+
+        static void printAr(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void buy(int[,] arr)
+        {
+            Console.WriteLine("Впишите, какое место хотите занять в кинотеатре(ряд, столбец): ");
+            string answer = Console.ReadLine();
+            string[] ans = answer.Split(',');
+            while (arr[Convert.ToInt32(ans[0]) - 1, Convert.ToInt32(ans[1]) - 1] != 0)
+            {
+                Console.WriteLine("Место занято. Введите другое");
+                answer = Console.ReadLine();
+                ans = answer.Split(',');
+            }
+            arr[Convert.ToInt32(ans[0]) - 1, Convert.ToInt32(ans[1]) - 1] = 3;
+            printAr(arr);
+        }
+    }
+}
---
+++ ex2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variant7ulesanne3
+{
+    class ex2
+    {
+        public static void Startex2()
+        {
+
+            int rowsAr = 1;
+            Random rnd = new Random();
+            int[] tempArr = new int[5];
+            do
+            {
+                Console.WriteLine("Enter the amount of rows: ");
+                rowsAr = Convert.ToInt32(Console.ReadLine());
+            } while (rowsAr % 2 != 0);
+            int[][] jagg = new int[rowsAr][];
+            for (int i = 0; i < rowsAr; i++)
+            {
+                jagg[i] = new int[5];
+                for (int j = 0; j < 5; j++)
+                {
+                    jagg[i][j] = rnd.Next(1, 15);
+                }
+            }
+            printAr(jagg);
+            for (int i = 0; i < jagg.Length; i = i + 2)
+            {
+                tempArr = jagg[i];
+                jagg[i] = jagg[i + 1];
+                jagg[i + 1] = tempArr;
+            }
+            Console.WriteLine();
+            printAr(jagg);
+            /*
+            int rows = 1;
+            do
+            {
+                Console.WriteLine("Enter the amount of rows: ");
+                rows = Convert.ToInt32(Console.ReadLine());
+            } while (rows % 2 != 0);
+            int[,] arr = new int[rows, 3];
+            int temp = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                arr[rnd.Next(arr.GetLength(0)), rnd.Next(arr.GetLength(1))] = rnd.Next(1,4);
+            }
+            printAr(arr);
+            for (int i = 0; i < arr.GetLength(0); i=i+2)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    temp = arr[i, j];
+                    arr[i, j] = arr[i + 1, j];
+                    arr[i + 1, j] = temp;
+                }
+            }
+            Console.WriteLine();
+            printAr(arr);*/
+        }
+        static void printAr(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void printAr(int[][] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    System.Console.Write("{0}{1}", arr[i][j], j == (arr[i].Length - 1) ? "" : " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
