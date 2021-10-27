2d414d76f0c2e61a92855e69843fcc44a71cb92b
opilane <opilane@HP-CZC6297B8L>
2021-10-27 10:28:21 +0300

    revamped  the whole thing, maybe works now

--- ex1.cs
+++ ex1.cs
@@ -59,7 +59,12 @@ namespace variant7ulesanne3
                 Console.Write($"Rida {i}: ");
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
+                    if (arr[i,j] == 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
                     Console.Write(arr[i, j]);
+                    Console.ResetColor();
                 }
                 Console.WriteLine();
             }
@@ -80,26 +85,109 @@ namespace variant7ulesanne3
             //printAr(arr);
 
             Console.WriteLine("Впишите, какое место хотите занять в кинотеатре(ряд): ");
-            int answer = Convert.ToInt32(Console.ReadLine());
+            int row = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Сколько мест вы хотите?: ");
             int amount = Convert.ToInt32(Console.ReadLine());
+            FromCentre(arr, amount, row);
         }
-        static void EmptyRows(int[,] arr, int amount, int row)
+        static void FromCentre(int[,] arr, int amount, int row)
         {
-            bool available = false;
-            int seats = Convert.ToInt32(Math.Round(Convert.ToDouble(arr.GetLength(1))/2));
-            for (int i = 0; i < arr.GetLength(1)/2; i++)
+            string dec = "";
+            int[] bought = new int[amount];
+            int seats = Convert.ToInt32(Math.Round(Convert.ToDouble((arr.GetLength(1)-amount)/ 2))) ;
+            Console.WriteLine(seats);
+            for (int i = 0; i < amount; i++)
             {
-                if (arr[row, seats+i] == 0)
+                if (arr[row, seats + i] == 0)
                 {
-                    for (int j = 0; j < amount; j++)
+                    bought[i] = seats + i;
+                }
+                else
+                {
+                    Array.Clear(bought, 0, bought.Length);
+                    Console.Write("К сожалению места в центре заняты. Хотите ли вы сами выбрать промежуток?(y/n): ");
+                    string dec1 = Console.ReadLine();
+                    if (dec1 == "y")
                     {
-                        if (arr[row, seats+i+j] != 0)
-                        {
-                            break;
-                        }
+                        UserChoice(arr, row);
+                        goto q;
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Всего доброго!");
+                        goto q;
                     }
                 }
+            }
+            Console.WriteLine("Вы уверены что хотите купить эти места?(y/n): ");
+            Console.WriteLine($"Rida {row}: ");
+            foreach (int i in bought)
+            {
+                Console.Write($"{i} ");
+            }
+            dec = Console.ReadLine();
+            if (dec == "y")
+            {
+                Console.WriteLine("Спасибо за покупку!");
+                foreach (var item in bought)
+                {
+                    arr[row, item] = 3;
+                }
+            }
+            else
+            {
+               Console.Write("Хотите ли вы выбрать места сами?(y/n): ");
+               dec = Console.ReadLine();
+                if (dec == "y")
+                {
+                    UserChoice(arr, row);
+                }
+                else
+                { Console.WriteLine("Всего доброго!"); }
+            }
+        q:;
+        }
+        static void UserChoice(int[,] arr, int row)
+        {
+            string dec = "";
+            Console.Write("Введите с какого места по какое хотите занять:(от, до) ");
+            string[] place = Console.ReadLine().Split(',');
+            int amount = Convert.ToInt32(place[1]) + Convert.ToInt32(place[0]);
+            int[] taken = new int[amount];
+            for (int i = 0; i < amount ; i++)
+            {
+                if (arr[row, Convert.ToInt32(place[0])] == 0)
+                {
+                    taken[i] = Convert.ToInt32(place[0]) + i;
+                }
+                else 
+                {
+                    Console.WriteLine("В вашем списке мест есть занятые места, хотите ли вы пропустить и занять следующее или отменить занимание впринципе?(skip/abort)");
+                    dec = Console.ReadLine();
+                    if (dec == "skip")
+                    {
+                        i++;
+                        taken[i] = 3;
+                    } else { return; }
+                }
+            }
+            Console.WriteLine("Вы уверены что хотите купить эти места?(y/n): ");
+            foreach (var item in taken)
+            {
+                Console.Write(item);
+            }
+            dec = Console.ReadLine();
+            if (dec == "y")
+            {
+                Console.WriteLine("Спасибо за покупку!");
+                foreach (var item in taken)
+                {
+                    arr[row, item] = 3;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Доброго дня!");
             }
         }
     }
